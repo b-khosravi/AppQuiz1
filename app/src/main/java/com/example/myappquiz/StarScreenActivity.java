7c2a86b6cfe1ec9225ff6d03b3c313007c20e617
b-khosravi <boshra68_kh@yahoo.com>
2020-01-05 16:48:51 +0330

    Initial commit

---
+++ app/src/main/java/com/example/myappquiz/StarScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.myappquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StarScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_screen);
+    }
+}
